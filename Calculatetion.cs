f70f64f0143ea9d0e78ef23465b19d553562ad68
Lukas Pedersen <lukas3302@hotmail.com>
2021-01-21 11:42:21 +0100

    FItness score wil now be displayed to the user

--- Calculatetion.cs
+++ Calculatetion.cs
@@ -8,5 +8,14 @@ namespace KonditalProgram_H2
 {
     class Calculatetion
     {
+        public static void CalculateFitness(int restingPulse, int maxPulse)
+        {
+            double Fitness = (double)maxPulse / (double)restingPulse;
+            double fitness2 = Fitness * 15.3;
+            Program.WriteToConsole($"Your physical condition score is: {String.Format("{0:0.0}", fitness2)}", 28);
+            Program.WriteToConsole("Press any key to try again", 28);
+            Console.ReadKey();
+            Program.Main();
+        }
     }
 }
